ba64eaad93109681c6153da517a4f1ce1e6e0c52
Kinka Sugimoto <kinka.sugimoto@cit.edu>
2025-03-23 19:51:07 +0800

    added get mapping for users

--- Backend/src/main/java/edu/cit/hapongo/controller/userController.java
+++ Backend/src/main/java/edu/cit/hapongo/controller/userController.java
@@ -1,8 +1,37 @@
 package edu.cit.hapongo.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
 import edu.cit.hapongo.model.User;
 import edu.cit.hapongo.service.UserService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import java.util.Optional;
 
+@RestController
+@RequestMapping("/users")
 public class UserController {
+
+    private UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    //Get all users
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+
+    //get user by name
+    @GetMapping("/name/{name}")
+    public Optional<User> getUserByName(@PathVariable String name) {
+        return userService.getUserByName(name);
+    }
     
 }
--- Backend/src/main/java/edu/cit/hapongo/service/userService.java
+++ Backend/src/main/java/edu/cit/hapongo/service/userService.java
@@ -7,6 +7,20 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    //Get all users
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    //Get user by name
+    public Optional<User> getUserByName(String name) {
+        return userRepository.findByName(name);
+    }
     
 }

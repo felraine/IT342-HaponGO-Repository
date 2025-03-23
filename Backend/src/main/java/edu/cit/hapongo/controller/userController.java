9489cdac0af90690c53946f9ee616d94190196d0
Kinka Sugimoto <kinka.sugimoto@cit.edu>
2025-03-23 19:17:23 +0800

    fixed capitalization of files UserController and UserService

--- Backend/src/main/java/edu/cit/hapongo/controller/userController.java
+++ Backend/src/main/java/edu/cit/hapongo/controller/userController.java
@@ -1,8 +1,8 @@
 package edu.cit.hapongo.controller;
 
 import edu.cit.hapongo.model.User;
-import edu.cit.hapongo.service.userService;
+import edu.cit.hapongo.service.UserService;
 
-public class userController {
+public class UserController {
     
 }
--- Backend/src/main/java/edu/cit/hapongo/service/userService.java
+++ Backend/src/main/java/edu/cit/hapongo/service/userService.java
@@ -7,6 +7,6 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
-public class userService {
+public class UserService {
     
 }

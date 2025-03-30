4e22758c60950593f6dde367d53a3691223a5c01
Kinka Sugimoto <kinka.sugimoto@cit.edu>
2025-03-30 16:55:04 +0800

    added the svg image for the login UI

--- frontend-web/HaponGO-web/src/components/Login.jsx
+++ frontend-web/HaponGO-web/src/components/Login.jsx
@@ -12,27 +12,34 @@ function Login() {
         <h1 className="title">HaponGO</h1>
       </header>
       
-      {/* Login Section */}
-      <div className="login-section">
-      <h1 className="second-title">Start your Japanese <br></br>Journey Today</h1>
-      <p>Learn Japanese the fun and easy way with HaponGO.</p>
+    {/* Login Container (Flexbox for side-by-side layout) */}
+    <div className="login-container">
+        {/* Login Section */}
+        <div className="login-section">
+          <h1 className="second-title">Start your Japanese <br />Journey Today</h1>
+          <p>Learn Japanese the fun and easy way with HaponGO.</p>
 
-      {/* Login Forms */}
-      <div className="login-form">
-        <form className="form">
-          <label className="label">Email</label>
-          <input type="text" className="input" /> <br />
-          <label className="label">Password</label>
-          <input type="password" className="input" /> <br />
-          <button type="submit" className="button">Sign in</button>
+          {/* Login Box */}
+          <div className="login-form">
+            <form className="form">
+              <label className="label">Email</label>
+              <input type="text" className="input" /> <br />
+              <label className="label">Password</label>
+              <input type="password" className="input" /> <br />
+              <button type="submit" className="button">Sign in</button>
 
-          {/* Sign up link area */}
-          <div className="signup-container">
-            <p className="signup-desc">Don't have an account? <a href="/register" className="signup-link">Sign up Here</a></p>
+              {/* Sign up section (inline) */}
+              <div className="signup-container">
+                <p className="signup-desc">
+                  Don't have an account? <a href="/register" className="signup-link">Sign up Here</a>
+                </p>
+              </div>
+            </form>
           </div>
-        </form>
+        </div>
 
-       </div>
+        {/* SVG Image on Right Side */}
+        <img src="/haponGO-image.svg" alt="HaponGO" className="haponGO-image" />
       </div>
     </>
   );

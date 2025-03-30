1850b1453d1bd08309686ef818ced23bcf4a2836
Kinka Sugimoto <kinka.sugimoto@cit.edu>
2025-03-30 16:38:50 +0800

    improved login UI

--- frontend-web/HaponGO-web/src/components/Login.jsx
+++ frontend-web/HaponGO-web/src/components/Login.jsx
@@ -11,10 +11,10 @@ function Login() {
       <header className="App-header">
         <h1 className="title">HaponGO</h1>
       </header>
-
+      
       {/* Login Section */}
       <div className="login-section">
-      <h1 className="second-title">Start your Japanese Journey Today</h1>
+      <h1 className="second-title">Start your Japanese <br></br>Journey Today</h1>
       <p>Learn Japanese the fun and easy way with HaponGO.</p>
 
       {/* Login Forms */}
@@ -24,13 +24,14 @@ function Login() {
           <input type="text" className="input" /> <br />
           <label className="label">Password</label>
           <input type="password" className="input" /> <br />
-          <button type="submit" className="button">Login</button>
+          <button type="submit" className="button">Sign in</button>
 
           {/* Sign up link area */}
           <div className="signup-container">
             <p className="signup-desc">Don't have an account? <a href="/register" className="signup-link">Sign up Here</a></p>
           </div>
         </form>
+
        </div>
       </div>
     </>

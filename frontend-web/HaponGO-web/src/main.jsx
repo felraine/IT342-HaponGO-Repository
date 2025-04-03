da5d41ead72cd30374bd2f55ecba419032eb729a
Kinka Sugimoto <kinka.sugimoto@cit.edu>
2025-04-04 00:15:44 +0800

    updated login UI using tailwindcss

--- frontend-web/HaponGO-web/src/main.jsx
+++ frontend-web/HaponGO-web/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './templates/index.css'
+import './index.css'
 import Login from './components/Login.jsx'
 
 createRoot(document.getElementById('root')).render(

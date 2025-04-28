9bd2300aaa9dfaba039bd59ba615691618d4c9a3
Rei Samson Agravante <samson.rei95@gmail.com>
2025-04-28 08:53:09 +0800

    Update Lesson.kt

--- frontend-mobile/AndroidStudioProjects/HaponGO/app/src/main/java/com/example/hapongo/model/Lesson.kt
+++ frontend-mobile/AndroidStudioProjects/HaponGO/app/src/main/java/com/example/hapongo/model/Lesson.kt
@@ -1,7 +1,9 @@
 package com.example.hapongo.model
 
+import java.io.Serializable
+
 data class Lesson(
     val lessonId: Long,
     val lessonName: String,
     val lessonOrder: Int
-)
+) : Serializable
